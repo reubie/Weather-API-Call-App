504fbb8d54865199652f2a59c71b7012b62d7f8f
reubie <r.nguyo27@gmail.com>
2021-10-04 16:27:36 +0300

    create click listeners for the layout buttons

--- app/src/main/java/com/example/weatherapicallapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapicallapp/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.weatherapicallapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_CityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        assigning values to each control on the layout
+
+        btn_CityID = findViewById(R.id.get_CityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherreports);
+
+//        click listeners for the buttons in the layout
+
+        btn_CityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "GET City", Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "GET ID", Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "GET Name", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
