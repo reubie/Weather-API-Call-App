a8300157b8d48c4b88880149077de5eebba41cb6
reubie <r.nguyo27@gmail.com>
2021-10-04 17:53:57 +0300

    create a JSON request to GET the api data

--- app/src/main/java/com/example/weatherapicallapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapicallapp/MainActivity.java
@@ -9,6 +9,16 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_CityID, btn_getWeatherByID, btn_getWeatherByName;
@@ -34,19 +44,55 @@ public class MainActivity extends AppCompatActivity {
         btn_CityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "GET City", Toast.LENGTH_LONG).show();
+
+
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query=london";
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Something Wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+                queue.add(request);
+//                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                               Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error 404", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//                // Add the request to the RequestQueue.
+
+//                Toast.makeText(MainActivity.this, "GET City", Toast.LENGTH_SHORT).show();
             }
         });
         btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "GET ID", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "GET ID", Toast.LENGTH_SHORT).show();
             }
         });
         btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "GET Name", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
